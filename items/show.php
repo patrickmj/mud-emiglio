08ba642bfda5fa2094088f033fa2df53c4fd4c70
patrickmj <patrickmjchnm@gmail.com>
2014-05-26 19:20:00 -0400

    Tweaks for MUD museums

--- items/show.php
+++ items/show.php
@@ -3,45 +3,54 @@
 <h1><?php echo metadata($item, array('Dublin Core', 'Title')); ?></h1>
 
 <div id="primary">
-    <!--  The following function prints all the the metadata associated with an item: Dublin Core, extra element sets, etc. See http://omeka.org/codex or the examples on items/browse for information on how to print only select metadata fields. -->
-    <?php echo all_element_texts($item); ?>
 
-    <?php fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item)); ?>
+    <div class='image mud'>
+    <?php echo file_markup($item->Files, array('imageSize' => 'fullsize')); ?>
+    </div>
+    <div class='element'>
+    <?php echo metadata($item, array('Dublin Core', 'Description')); ?>
+    </div>
+
+    <div class='element'>
+    <?php $wikipediaUrl = metadata($item, array('MUD Elements', 'Wikipedia Url')); ?>
+    <?php if(empty($wikipediaUrl)): ?>
+    <p>
+    There is no Wikipedia entry for this museum. Please foster greater exchange of our shared cultural heritage by <a href='http://en.wikipedia.org/wiki/Wikipedia:Starting_an_article'>creating a wikipedia page</a> for it.
+    </p>
+    <?php else:?>
+    <a href="<?php echo $wikipediaUrl; ?>">Wikipedia Page</a>
+    <?php endif; ?>
+    </div>
+    <div class='element'>
+    <?php $dbpediaUri = metadata($item, array('MUD Elements', 'DBpedia Uri')); ?>
+    <?php if(!empty($dbpediaUri)):?>
+    <p>Linked Open Data URI: <?php echo url_to_link($dbpediaUri); ?>.</p>
+    <p>Learn more about <a href='http://en.wikipedia.org/wiki/Linked_data'>Linked Open Data</a>.
+    </p>
+    <?php endif;?>
+    </div>
+    
 
 </div><!-- end primary -->
 
 <div id="secondary">
 
-    <!-- The following returns all of the files associated with an item. -->
-    <?php if (metadata($item, 'has files')): ?>
-    <div id="itemfiles" class="element">
-        <h2>Files</h2>
-        <div class="element-text"><?php echo files_for_item(); ?></div>
+    <div class="link">
+    <a href="<?php echo metadata($item, array('MUD Elements', 'WEBURL')); ?>"><?php echo metadata($item, array('Dublin Core', 'Title'));?></a>
     </div>
-    <?php endif; ?>
-
-    <!-- The following prints a list of all tags associated with the item -->
-    <?php if (metadata($item, 'has tags')): ?>
-    <div id="item-tags" class="element">
-        <h2>Tags</h2>
-        <div class="element-text tags"><?php echo tag_string('item'); ?></div>
+    <div class="address">
+        <?php echo metadata($item, array('MUD Elements', 'ADDRESS')); ?>
+        <br />
+        <?php echo metadata($item, array('MUD Elements', 'CITY')); ?>,
+        <?php echo metadata($item, array('MUD Elements', 'STATE')); ?>
+        <?php echo metadata($item, array('MUD Elements', 'ZIP')); ?>
     </div>
-    <?php endif; ?>
-
-    <!-- If the item belongs to a collection, the following creates a link to that collection. -->
-    <?php if (get_collection_for_item()): ?>
-        <div id="collection" class="element">
-            <h2>Collection</h2>
-            <div class="element-text"><p><?php echo link_to_collection_for_item(); ?></p></div>
-        </div>
-    <?php endif; ?>
-
-    <!-- The following prints a citation for this item. -->
-    <div id="item-citation" class="element">
-        <h2>Citation</h2>
-        <div class="element-text"><?php echo metadata('item', 'citation', array('no_escape' => true)); ?></div>
+    <div class="phone">
+        <?php echo metadata($item, array('MUD Elements', 'PHONE')); ?>
     </div>
-
+<?php fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item)); ?>
+    
+    
 </div><!-- end secondary -->
 
 <ul class="item-pagination navigation">
